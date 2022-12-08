43cdf44455647725225eebc52918db642e2c8577
smitch5777 <36050924+smitch5777@users.noreply.github.com>
2022-12-08 16:32:45 -0700

    Update ForSale.js

--- front-end/src/pages/ForSale.js
+++ front-end/src/pages/ForSale.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './css/forSale.css';
+import './css/favorites.css';
 import Duck from './components/Duck';
 
 const ForSale = () => {
@@ -13,18 +13,17 @@ const ForSale = () => {
             const response = await axios.get("/api/ducks");
             console.log('fetchDucks response = ' + JSON.stringify(response));
             console.log('fetchDucks data = ' + JSON.stringify(response.data));
-            console.log('fetchDucks products = ' + JSON.stringify(response.data));
+            console.log('url = ' + response.data[0].img_url);
             setDucks(response.data);
         } catch (error) {
             setError("error retrieving ducks: " + error);
         }
     }
 
-    const addToFavorites = async (e, id) => {
+    const addToFavorites = async (id) => {
         try {
             const response = await axios.put("/api/favorites/" + id);
             console.log('Add To Favorites response = ' + JSON.stringify(response));
-            alert('This duck successfully added to favorites');
         } catch (error) {
             setError("error retrieving ducks: " + error);
         }
@@ -35,17 +34,18 @@ const ForSale = () => {
     }, []);
 
     return (
-        <div className="DucksForSale">
-            <h1>Ducks For Sale</h1>
+        <div className="FavoriteDucks">
+            <div className="TitleText">Ducks For Sale</div>
             {ducks.length > 0 ?
                 ducks?.map(duck => (
-                    <div>
+                    <div className='oneDuck'>
                         <Duck duck={duck} setError={setError} />
-                        <button className="like" onClick={addToFavorites}>Add to Favorites</button>
+                        <button className="dislike" onClick={() => addToFavorites(duck.id)}>Add to Favorites</button>
                     </div>
                 )) :
                 <div>I'm sorry, it appears we don't have any Ducks for Sale.</div>
             }
+            {console.log('length: ' + ducks.length + '\n ducks: ' + JSON.stringify(ducks))}
         </div>
     )
 }
